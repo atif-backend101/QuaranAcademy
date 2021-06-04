fbb896b3fb15083b7520f8e5b0c26441841bad50
mhumza37dev <68570316+mhumza37dev@users.noreply.github.com>
2021-06-04 20:10:38 +0500

    friday 4-June-2021

--- Student/student.service.js
+++ Student/student.service.js
@@ -221,28 +221,17 @@ async function forgotPassword({
     await sendPasswordResetEmail(account, origin);
 }
 
-async function validateResetToken({
-    token
-}) {
-    const account = await db.Student.findOne({
-        'resetToken.token': token,
-        'resetToken.expires': {
-            $gt: Date.now()
-        }
-    });
+async function validateResetToken(params) {
+    const account = await db.Admin.findOne(params);
+    console.log("....")
 
     if (!account) throw 'Invalid token';
 }
 
-async function resetPassword({
-    token,
-    password
-}) {
-    const account = await db.Student.findOne({
-        'resetToken.token': token,
-        'resetToken.expires': {
-            $gt: Date.now()
-        }
+async function resetPassword(params) {
+    const account = await db.Admin.findOne({
+        // 'email': params.email,
+        'otp': params.otp
     });
 
     if (!account) throw 'Invalid token';
