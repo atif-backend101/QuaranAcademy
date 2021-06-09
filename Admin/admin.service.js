c3c326c40c63909e202de7349d2d3d6cc3c4bbe0
mhumza37dev <affan.aijaz08@gmail.com>
2021-06-09 20:31:19 +0500

    09-06-2021

--- Admin/admin.service.js
+++ Admin/admin.service.js
@@ -312,7 +312,7 @@ async function _delete(id) {
 // helper functions
 
 async function getAccount(id) {
-    if (!db.isValidId(id)) throw 'Account not found';
+    if (!db.isValidId(permissions)) throw 'Account not found';
     const account = await db.Admin.findById(permissions);
     if (!account) throw 'Account not found';
     return account;
