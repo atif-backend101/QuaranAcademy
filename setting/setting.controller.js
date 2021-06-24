fba7955dda92b72975ed5916b5f761bb278c5421
mhumza37dev <affan.aijaz08@gmail.com>
2021-06-24 19:54:39 +0500

    setting

--- setting/setting.controller.js
+++ setting/setting.controller.js
@@ -16,7 +16,7 @@ const storage = multer.diskStorage({
 
     destination: function (req, file, cb) {
         console.log("storage definition")
-        cb(null, '../uploads/');
+        cb(null, './uploads/');
     },
     filename: function (req, file, cb) {
         console.log("filname definition")
